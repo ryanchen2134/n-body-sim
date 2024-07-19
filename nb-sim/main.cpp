debd1719bd252637f3dc6f46eb7091c980a0ebe5
Ryan <chen.ryan1234@outlook.com>
2024-07-19 13:57:54 -0700

    Refactoring, Readability, and Scope Handling

--- nb-sim/main.cpp
+++ nb-sim/main.cpp
@@ -1,3 +1,11 @@
+/**
+ * @file main.cpp
+ * @brief This file contains the main function for the n-body simulation program.
+ * The program simulates the motion of mass objects in a 3D space using the n-body simulation algorithm.
+ * It creates a shared memory region to store the simulation data and uses a semaphore for synchronization.
+ * The simulation runs in an infinite loop, updating the positions and velocities of the mass objects in each frame.
+ * The user can specify the time interval between frames.
+ */
 #include <iostream>
 #include <vector>
 #include <fcntl.h>
@@ -13,27 +21,129 @@
 #include "MassObject.hpp"
 #include "utilities.hpp"
 
-sem_t* g_sem = nullptr;
-const char* g_sem_name = nullptr;
-void* g_shm_ptr = nullptr;
-const char* g_shm_name = nullptr;
-size_t g_shm_size = 0;
+//Reliance on Global Variables for resource management operations solely for ease of cleanup
 
-void* alloc_shm_space(const size_t shm_size, const char* shm_name);
+//Semaphore (Mutex)
+sem_t* SEM = nullptr;
+constexpr char* SEM_NAME = "/sim_semaphore";
+//SHared Memory
+void* SHM_PTR = nullptr;
+constexpr char* SHM_NAME = "/vector_shared_memory";
+size_t SHM_SIZE = 0;
+
+void* alloc_shm_space();
+void cleanup();
+void signal_handler(int signum);
+double get_user_time_interval();
 
 struct Metadata {
     size_t object_count;
     std::atomic<size_t> frame;
 };
 
-void cleanup() {
-    if (g_shm_ptr != nullptr) {
-        munmap(g_shm_ptr, g_shm_size);
-        shm_unlink(g_shm_name);
+
+/**
+ * @brief The main function of the program.
+ * 
+ * This function is the entry point of the program. It sets up a signal handler for SIGINT,
+ * creates a semaphore, initializes mass objects, allocates shared memory space, and starts
+ * the simulation loop. The simulation loop runs indefinitely, updating the object states,
+ * incrementing the frame count, and printing the object states. The loop can be interrupted
+ * by pressing Ctrl+C.
+ * 
+ * @return 0 indicating successful execution of the program.
+ */
+int main() {
+    signal(SIGINT, signal_handler);
+
+    SEM = sem_open(SEM_NAME, O_CREAT, 0644, 1);
+    if (SEM == SEM_FAILED) {
+        throw std::runtime_error("Failed to create semaphore");
     }
-    if (g_sem != nullptr) {
-        sem_close(g_sem);
-        sem_unlink(g_sem_name);
+
+    double time_interval = get_user_time_interval(); //given by user input
+    
+
+    MassObject a(10000, {1, 0, 0}, {0, 0, 0}, time_interval);
+    MassObject b(10000, {-1, 0, 0}, {0, 0, 0});
+    MassObject c(10000, {0, 0, 0}, {0, 0, 0});
+
+    const size_t object_count = MassObject::objects.size();
+    const size_t header_size = sizeof(Metadata) + sizeof(double) * object_count;
+    const size_t frame_size = object_count * sizeof(double) * 6;
+    SHM_SIZE = header_size + frame_size;
+    SHM_PTR = alloc_shm_space();
+
+    std::cout << "Shared memory created and mapped successfully." << std::endl;
+
+
+    //Memory Structure Begins Here
+    Metadata* metadata = static_cast<Metadata*>(SHM_PTR);
+              metadata->object_count = object_count;
+              metadata->frame = 1;
+
+    double* data_ptr = reinterpret_cast<double*>(reinterpret_cast<uint8_t*>(SHM_PTR) + header_size);
+
+    //Copy Masses
+    for (size_t i = 0; i < object_count; ++i) {
+        memcpy(reinterpret_cast<uint8_t*>(SHM_PTR) + sizeof(Metadata) + i * sizeof(double), &MassObject::objects[i].mass, sizeof(double));
+    }
+
+    
+    //Begin Simulation Code
+
+    print_object_state_all();
+    while(true) {
+        std::cout << "Frame: " << metadata->frame << std:: endl;
+        run_one_frame();
+        sem_wait(SEM);
+        ++metadata->frame;
+        for (size_t i = 0; i < object_count; ++i) {
+            memcpy(reinterpret_cast<uint8_t*>(data_ptr) + i * 6 * sizeof(double), MassObject::objects[i].position.data(), 3 * sizeof(double));
+            memcpy(reinterpret_cast<uint8_t*>(data_ptr) + i * 6 * sizeof(double) + 3 * sizeof(double), MassObject::objects[i].velocity.data(), 3 * sizeof(double));
+        }
+        sem_post(SEM);
+        print_object_state_all();
+    }
+
+    cleanup();
+    return 0;
+}
+
+/// @brief 
+
+/// @return 
+void* alloc_shm_space() {
+    int shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
+    if (shm_fd == -1) {
+        throw std::runtime_error("Failed to create shared memory");
+    }
+
+    if (ftruncate(shm_fd, SHM_SIZE) == -1) {
+        close(shm_fd);
+        shm_unlink(SHM_NAME);
+        throw std::runtime_error("Failed to set size of shared memory");
+    }
+
+    void* shm_ptr = mmap(nullptr, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (shm_ptr == MAP_FAILED) {
+        close(shm_fd);
+        shm_unlink(SHM_NAME);
+        throw std::runtime_error("Failed to map shared memory");
+    }
+
+    return shm_ptr;
+}
+
+
+void cleanup() {
+    if (SHM_PTR != nullptr) {
+        munmap(SHM_PTR, SHM_SIZE);
+        shm_unlink(SHM_NAME);
+    }
+    if (SEM != nullptr) {
+        sem_close(SEM);
+        sem_unlink(SEM_NAME);
     }
 }
 
@@ -43,15 +153,7 @@ void signal_handler(int signum) {
     exit(signum);
 }
 
-int main() {
-    signal(SIGINT, signal_handler);
-
-    g_sem_name = "/sim_semaphore";
-    g_sem = sem_open(g_sem_name, O_CREAT, 0644, 1);
-    if (g_sem == SEM_FAILED) {
-        throw std::runtime_error("Failed to create semaphore");
-    }
-
+double get_user_time_interval(){
     double time_interval;
     bool valid_input = false;
     while (!valid_input) {
@@ -64,70 +166,5 @@ int main() {
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
     }
-
-    MassObject a(10000, {1, 0, 0}, {0, 0, 0}, time_interval);
-    MassObject b(10000, {-1, 0, 0}, {0, 0, 0});
-    MassObject c(10000, {0, 0, 0}, {0, 0, 0});
-
-    const size_t n = MassObject::objects.size();
-    const size_t header_size = sizeof(Metadata) + sizeof(double) * n;
-    const size_t frame_size = n * sizeof(double) * 6;
-    const size_t shm_size = header_size + frame_size;
-    g_shm_size = shm_size;
-    g_shm_name = "/vector_shared_memory";
-    g_shm_ptr = alloc_shm_space(shm_size, g_shm_name);
-
-    std::cout << "Shared memory created and mapped successfully." << std::endl;
-
-    Metadata* metadata = static_cast<Metadata*>(g_shm_ptr);
-    metadata->object_count = n;
-    metadata->frame = 1;
-
-    double* data_ptr = reinterpret_cast<double*>(reinterpret_cast<uint8_t*>(g_shm_ptr) + header_size);
-
-    for (size_t i = 0; i < n; ++i) {
-        memcpy(reinterpret_cast<uint8_t*>(g_shm_ptr) + sizeof(Metadata) + i * sizeof(double), &MassObject::objects[i].mass, sizeof(double));
-    }
-
-    print_object_state_all();
-
-    while(true) {
-        std::cout << "Frame: " << metadata->frame << std:: endl;
-        run_one_frame();
-        sem_wait(g_sem);
-        ++metadata->frame;
-        for (size_t i = 0; i < n; ++i) {
-            memcpy(reinterpret_cast<uint8_t*>(data_ptr) + i * 6 * sizeof(double), MassObject::objects[i].position.data(), 3 * sizeof(double));
-            memcpy(reinterpret_cast<uint8_t*>(data_ptr) + i * 6 * sizeof(double) + 3 * sizeof(double), MassObject::objects[i].velocity.data(), 3 * sizeof(double));
-        }
-        sem_post(g_sem);
-        print_object_state_all();
-    }
-
-    std::cout << "Simulation complete." << std::endl;
-    cleanup();
-    std::cout << "Clean up done. Exiting." << std::endl;
-    return 0;
-}
-
-void* alloc_shm_space(const size_t shm_size, const char* shm_name) {
-    int shm_fd = shm_open(shm_name, O_CREAT | O_RDWR, 0666);
-    if (shm_fd == -1) {
-        throw std::runtime_error("Failed to create shared memory");
-    }
-
-    if (ftruncate(shm_fd, shm_size) == -1) {
-        close(shm_fd);
-        shm_unlink(shm_name);
-        throw std::runtime_error("Failed to set size of shared memory");
-    }
-
-    void* shm_ptr = mmap(nullptr, shm_size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
-    if (shm_ptr == MAP_FAILED) {
-        close(shm_fd);
-        shm_unlink(shm_name);
-        throw std::runtime_error("Failed to map shared memory");
-    }
-
-    return shm_ptr;
+    return time_interval;
 }
